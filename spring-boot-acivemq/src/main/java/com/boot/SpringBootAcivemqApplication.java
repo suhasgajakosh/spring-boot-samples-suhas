3ca1c79c40bf9d8600f29c01d20f928045ebd7dc
Gajakosh <suhas.gajakosh@capgemini.com>
2016-11-22 12:26:01 +0530

    spring-boot-smaples

---
+++ spring-boot-acivemq/src/main/java/com/boot/SpringBootAcivemqApplication.java
@@ -0,0 +1,23 @@
+package com.boot;
+
+import javax.jms.Queue;
+
+import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+
+@SpringBootApplication
+@EnableJms
+public class SpringBootAcivemqApplication {
+	
+	@Bean
+	public Queue queue() {
+		return new ActiveMQQueue("sample.queue");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAcivemqApplication.class, args);
+	}
+}
